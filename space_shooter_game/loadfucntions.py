e80b8fb582213be261838dad7fa635b41dbee901
sh3rawy <mhmdelbaset404@gmail.com>
2025-05-22 17:39:57 +0300

    fixed path_load

--- space_shooter_game/loadfucntions.py
+++ space_shooter_game/loadfucntions.py
@@ -2,4 +2,4 @@ import os
 
 working_folder = "space_shooter_game"
 
-path_load = lambda path: f"{os.getcwd()}/{working_folder}/path"
+path_load = lambda path: f"{os.getcwd()}/{working_folder}/{path}"
