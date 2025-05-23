d5eb89f2b6cda8aa7b966a553c37880f731c6945
sh3rawy <mhmdelbaset404@gmail.com>
2025-05-23 11:06:47 +0300

    loadfunctions.py edited professionaly using os.path.exist() function

--- space_shooter_game/loadfucntions.py
+++ space_shooter_game/loadfucntions.py
@@ -1,9 +1,10 @@
 import os
 
-working_folder = "space_shooter_game"
-
-def path_load(path:str) -> str:
-    try:
-        return f"{path}"                                             # for windows users
-    except FileNotFoundError:
-        return f"{os.getcwd()}/{working_folder}/{path}"
+def path_load(path: str) -> str:
+    working_folder = "space_shooter_game"
+    if os.path.exists(path):
+        return path
+    full_path = os.path.join(working_folder, path)
+    if os.path.exists(full_path):
+        return full_path
+    raise FileNotFoundError(f"File not found in '{path}' or '{full_path}'")
