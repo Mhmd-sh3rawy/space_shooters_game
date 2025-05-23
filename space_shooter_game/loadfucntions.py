9e9a3349815108426fe9cdfc171613b5ff01df08
sh3rawy <mhmdelbaset404@gmail.com>
2025-05-23 10:09:00 +0300

    fixed FileNotFoundError for windows

--- space_shooter_game/loadfucntions.py
+++ space_shooter_game/loadfucntions.py
@@ -4,6 +4,6 @@ working_folder = "space_shooter_game"
 
 def path_load(path:str) -> str:
     try:
-        return f"{os.getcwd()}\{working_folder}\{path}"
+        return f"{path}"                                             # for windows users
     except FileNotFoundError:
-        return f"{os.getcwd()}/{working_folder}/{path}"
+        return f"{os.getcwd()}/{working_folder}/{path}"                            # for linux users
